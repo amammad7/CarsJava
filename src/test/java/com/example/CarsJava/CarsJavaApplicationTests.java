c897aed029a7b6dbc02b2e55074725b8e02aa786
Ali <amammad7@stu.vistula.edu.pl>
2025-05-20 18:10:23 +0200

    Task 1

---
+++ src/test/java/com/example/CarsJava/CarsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
