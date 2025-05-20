84a581f30aa6dbf5f5c06faecc55beb3ed12e0fe
Ali <amammad7@stu.vistula.edu.pl>
2025-05-20 18:24:53 +0200

    Task 1

--- src/main/java/com/example/CarsJava/controller/HelloController.java
+++ src/main/java/com/example/CarsJava/controller/HelloController.java
@@ -1,15 +1,15 @@
 package com.example.CarsJava.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring Boot!";
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("message", "BMW M3");
+        return "greeting"; // corresponds to greeting.html
     }
 }
